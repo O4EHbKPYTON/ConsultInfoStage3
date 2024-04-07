3858f381599be3f4f542445a1121bd4f14d46795
O4EHbKPYTON <yan.matveyev@outlook.com>
2024-04-07 19:38:46 +0200

    stage 3 completed

---
+++ local/modules/up.users.group/lang/ru/install/index.php
@@ -0,0 +1,6 @@
+<?php
+$MESS ['UP_USERS_GROUP_MODULE_NAME'] = "users.group";
+$MESS ['UP_USERS_GROUP_MODULE_DESCRIPTION'] = "Модуль для вывода информации о группе пользователей";
+$MESS ['UP_USERS_GROUP_INSTALL_TITLE'] = "Установка модуля";
+$MESS ['UP_USERS_GROUP_UNINSTALL_TITLE'] = "Удаление модуля";
+$MESS ['UP_USERS_GROUP_DENIED'] = "Доступ закрыт";
